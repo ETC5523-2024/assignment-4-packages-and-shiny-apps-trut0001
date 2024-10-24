38c62a078c727a62fbe0a00cc9f2bfaa85c029dd
Travis Rutledge <trut0001@student.monash.edu>
2024-10-24 19:08:53 +1100

    push before doing part c

--- usfederaltaxexplorer/R/run_app.R
+++ usfederaltaxexplorer/R/run_app.R
@@ -1,8 +1,12 @@
 #' Launch the Shiny App
 #'
-#' This function launches the Shiny app included in the package.
+#' This function launches the Shiny app for exploring how US federal taxes are distributed.
 #'
-#' @return A Shiny app object
+#' @return A Shiny app object.
+#' @examples
+#' if (interactive()) {
+#'   usfederaltaxexplorer::run_app()
+#' }
 #' @export
 run_app <- function() {
   shiny::runApp(system.file("shinyapp", "app.R", package = "usfederaltaxexplorer"))
